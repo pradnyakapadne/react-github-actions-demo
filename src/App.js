9cf383603ab04c3e51e44f143dc5ac672d158a0f
Pradnya Kapadne <88652856+pradnyakapadne@users.noreply.github.com>
2026-06-04 15:27:58 +0530

    renamed headline

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ export default function App() {
 									fontFamily: `Greycliff CF, ${theme.fontFamily}`,
 									fontWeight: 900,
 								})}>
-								My Tasks
+								My Tasks_1
 							</Title>
 							<ActionIcon
 								color={'blue'}
